43c959389f7006d16a9822c0f228371b38e62af0
John Doe <johndoe@example.com>
2015-11-25 18:25:03 -0800

    first commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,16 @@
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:notice] = 'Thank you for your message. I will contact you soon!'
+    else
+      flash.now[:error] = 'Could not send message. Please contact me directly at wonjohnsoup@gmail.com. Thank you!'
+      render :new
+    end
+  end
+end
